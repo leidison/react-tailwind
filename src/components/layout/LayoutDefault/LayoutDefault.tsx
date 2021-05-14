2ba2aa922993fc93c460c14d2e67f3ba69693f37
Leidison Siqueira <leidison.sb@gmail.com>
2021-05-14 00:56:47 -0300

    Menu de teste implementado

--- src/components/layout/LayoutDefault/LayoutDefault.tsx
+++ src/components/layout/LayoutDefault/LayoutDefault.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react'
+import TopBar from '@components/layout/TopBar/TopBar'
 
 const LayoutDefault: FC<{ children: any }> = ({ children }) => {
-  return <main>{children}</main>
+  return <main>
+
+    <TopBar />
+
+    {children}
+
+  </main>
 }
 
 export const getLayout = (page: FC) => {
